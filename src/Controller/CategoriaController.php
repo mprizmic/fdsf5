c1eae123b124be21322caab960168de47738a8dc
mprizmic <mprizmic@gmail.com>
2021-12-16 17:27:44 -0300

    avance curso categoria nueva

--- src/Controller/CategoriaController.php
+++ src/Controller/CategoriaController.php
@@ -2,22 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Categoria;
 use App\Repository\CategoriaRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/categoria")
+ */    
 class CategoriaController extends AbstractController
 {
     /**
-     * @Route("/categoria", name="categoria")
+     * @Route("/listado", name="app_listado_categoria")
      */
     public function index(CategoriaRepository $categoriaRepository): Response
     {
         $categorias = $categoriaRepository->findAll();
-        dump($categorias);
+
+        dump($categorias);die();
+
         return $this->render('categoria/index.html.twig', [
             'controller_name' => 'CategoriaController',
+
         ]);
+    }
+    /**
+     * @Route("/nueva", name="app_nueva_categoria")
+     */    
+    public function nueva(CategoriaRepository $categoriaRepository , EntityManagerInterface $entityManager, Request $request) 
+    {
+
+        $categoria = new Categoria();
+
+        if ($this->isCsrfTokenValid('categoria', $request->request->get('_token'))){
+            $nombre = $request->request->get('nombre', null);
+            $categoria->setNombre($nombre);
+            if ($nombre){
+                $entityManager->persist($categoria);
+                $entityManager->flush();
+                $this->addFlash('success', 'Categoria creada correctamente');
+                return $this->redirectToRoute('app_listado_categoria');
+            }
+
+
+        }
+
+        return $this->render('categoria/nueva.html.twig', [
+            'categoria' => $categoria,
+
+        ]); 
     }
 }
