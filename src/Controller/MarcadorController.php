78e252d1050d59b169f40379b0c27cf8cf69c1ec
mprizmic <mprizmic@gmail.com>
2021-12-20 18:52:24 -0300

    crud automatico

---
+++ src/Controller/MarcadorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Marcador;
+use App\Form\MarcadorType;
+use App\Repository\MarcadorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/marcador")
+ */
+class MarcadorController extends AbstractController
+{
+    /**
+     * @Route("/", name="marcador_index", methods={"GET"})
+     */
+    public function index(MarcadorRepository $marcadorRepository): Response
+    {
+        return $this->render('marcador/index.html.twig', [
+            'marcadors' => $marcadorRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="marcador_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $marcador = new Marcador();
+        $form = $this->createForm(MarcadorType::class, $marcador);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($marcador);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('marcador_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('marcador/new.html.twig', [
+            'marcador' => $marcador,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="marcador_show", methods={"GET"})
+     */
+    public function show(Marcador $marcador): Response
+    {
+        return $this->render('marcador/show.html.twig', [
+            'marcador' => $marcador,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="marcador_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Marcador $marcador, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MarcadorType::class, $marcador);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('marcador_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('marcador/edit.html.twig', [
+            'marcador' => $marcador,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="marcador_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Marcador $marcador, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$marcador->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($marcador);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('marcador_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Marcador.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MarcadorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MarcadorRepository::class)
+ */
+class Marcador
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Categoria::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categoria;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCategoria(): ?Categoria
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(?Categoria $categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+}
