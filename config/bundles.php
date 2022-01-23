1a64d6341f3a338206a9f8b006da7d76af3e5002
mprizmic <mprizmic@gmail.com>
2022-01-22 22:21:33 -0300

    primer commit de sesion con usuario

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
